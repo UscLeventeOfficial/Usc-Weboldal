89c404322d5ebca07dcf504f2ef347b01da1a415
Hegedűs Levente <uscleventeuscv@gmail.com>
2024-10-02 17:49:58 +0200

    Sok minden

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 window.addEventListener("scroll", function() {
-    const backToTopButton = document.querySelector(".back-to-top");
-    if (window.pageYOffset > 300) { // 300px görgetés után jelenik meg
+    const backToTopButton = document.querySelector(".vissza-a-tetejere");
+    if (window.pageYOffset > 300) { 
         backToTopButton.classList.add("show");
     } else {
         backToTopButton.classList.remove("show");
     }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const hideButton = document.getElementById("eltuntet");
+    const video = document.getElementById("Video");
+    const gomb = document.getElementById("eltuntet")
+
+    hideButton.addEventListener("click", function() {
+        video.style.display = "none"; 
+        gomb.style.display = "none"
+    });
 });
