582356e6636ae06ec19cf79e93d617ab27209d14
UscLeventeOfficial <uscleventeusc@gmail.com>
2024-10-02 11:00:51 +0200

    gomb

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener("scroll", function()
+{
+    const visszaTetejere = document.querySelector(".vissza-a-tetejere");
+        if(window.pageYOffset > 300)
+    visszaTetejere.classList.add        
+
+}
